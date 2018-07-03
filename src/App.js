b1c98c0895a245a46ad7c4f83ec06384b0765577
Brian C Sparks <brian.c.sparks@hp.com>
2018-07-03 13:28:20 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader'
-import logo from './logo.svg';
+import {
+  Nav, Navbar, Grid,
+  // NavDropdown,
+  // MenuItem
+}                             from 'react-bootstrap';
+
+import './short.css'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar inverse fluid fixedTop>
+
+          {/* Header and Brand */}
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Netlab Telemetry Viewer</a>
+            </Navbar.Brand>
+
+          </Navbar.Header>
+
+          <Navbar.Collapse>
+
+            <Nav>
+              {/* <NavDropdown eventKey={1} title="Actions" id="action-dropdown">
+                <MenuItem eventKey={1.1} onSelect={onSelectAction}>Show Data Types in Console</MenuItem>
+              </NavDropdown> */}
+
+              {/* <ItemList itemType="clients"  itemKeyName="clientId" />
+              <ItemList itemType="sessions" itemKeyName="sessionId" /> */}
+
+              <Navbar.Text pullRight>
+              Luigi
+                {/* {`${displaySessionId(sessionId)}`} */}
+              </Navbar.Text>
+            </Nav>
+
+            {/* <Nav pullRight>
+              <NetworkActivity />
+            </Nav> */}
+          </Navbar.Collapse>
+        </Navbar>
+
+        {/* Main Content Area */}
+        <div>
+          <Grid fluid={true}>
+            {/* <TopTabs>
+            </TopTabs> */}
+
+            Mario
+
+          </Grid>
+        </div>
+
       </div>
     );
   }
